aca2b4fb37e90d7fdb6c1d3b93f66e8a09de5fad
sealvlon <sealvlon@gmail.com>
2026-01-20 14:27:04 -0500

    iOS
    iOS

---
+++ iOS App/Tyches/TychesTests/TychesTests.swift
@@ -0,0 +1,17 @@
+//
+//  TychesTests.swift
+//  TychesTests
+//
+//  Created by Sebastián Álvarez Londoño on 12/3/25.
+//
+
+import Testing
+@testable import Tyches
+
+struct TychesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
