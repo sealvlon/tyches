e47f8ff67362a4755ac15004c6718338e0994ff2
sealvlon <sealvlon@gmail.com>
2026-01-19 22:05:50 -0500

    iOS
    iOS

---
+++ web/iOS App/Tyches/TychesUITests/TychesUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  TychesUITestsLaunchTests.swift
+//  TychesUITests
+//
+//  Created by Sebastián Álvarez Londoño on 12/3/25.
+//
+
+import XCTest
+
+final class TychesUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
